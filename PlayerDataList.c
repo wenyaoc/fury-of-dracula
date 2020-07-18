d82b1b2a18083bb2335be3365216cf9443bfb4f8
wenyaoc <wenyaochen427@gmail.com>
2020-07-18 20:15:08 +1000

    update player data ADT

---
+++ PlayerDataList.c
@@ -0,0 +1,37 @@
+#include <assert.h>
+#include <stdio.h>
+
+#include "Game.h"
+#include "PlayerDataList.h"
+#include "Places.h"
+#include "Map.h"
+
+/*
+void ListFree(historyNode node) {
+
+}
+
+void NodeFree(historyNode node) {
+
+}
+
+HistoryNode newNode(PlaceId place, bool vampire, bool trap, bool revealed){
+    return NULL;
+}
+
+// add the node to the head of the list  
+playerData addToHead(playerData data, HistoryNode newNode){
+    return NULL;
+}
+
+// add the node to the end of the list
+playerData addToTail(playerData data, HistoryNode newNode){
+    return NULL;
+}
+
+// get a copy of the last N Node in the history list
+// returns: the head of the new list
+// !!!!!! make sure you free it after you use it !!!!!!
+HistoryNode getLastN(playerData data, int n){
+    return NULL;
+}*/
