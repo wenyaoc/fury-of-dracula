cc7aefbbeef2850ae13d0b6042b89d863f6237c8
wenyaoc <wenyaochen427@gmail.com>
2020-07-30 03:11:48 +1000

    stupid hunters

--- hunt/hunter.c
+++ hunt/hunter.c
@@ -13,8 +13,112 @@
 #include "hunter.h"
 #include "HunterView.h"
 
-void decideHunterMove(HunterView hv)
-{
+
+#include "Places.h"
+#include <string.h>
+/*
+typedef mode {
+	CHASING,	
+	RANDOM,	
+	RESEARCH	
+} Mode;
+*/
+const char * decideLordGodalmingMove(HunterView hv);
+const char * decideDrSewardMove(HunterView hv);
+const char * decideVanHelsingMove(HunterView hv);
+const char * decideMinaHarkerMove(HunterView hv);
+PlaceId predictLocation(HunterView hv);
+
+void decideHunterMove(HunterView hv) {
 	// TODO: Replace this with something better!
-	registerBestPlay("TO", "Have we nothing Toulouse?");
+	Player player = HvGetPlayer(hv);
+	const char * place;
+    switch (player) {
+        case PLAYER_LORD_GODALMING:
+            place = decideLordGodalmingMove(hv);
+        break;
+        case PLAYER_DR_SEWARD:
+            place = decideDrSewardMove(hv);
+        break;
+        case PLAYER_VAN_HELSING:
+            place = decideVanHelsingMove(hv);
+        break;
+        case PLAYER_MINA_HARKER:
+            place = decideMinaHarkerMove(hv);
+        break;
+		default:
+		break;
+    }
+
+	char * newPlace = malloc(2 * sizeof(char));
+	strcpy(newPlace, place);
+	registerBestPlay(newPlace, "I like CASTLE_DRACULA");
 }
+
+
+const char * decideLordGodalmingMove(HunterView hv) {
+	PlaceId currPlace = HvGetPlayerLocation(hv, PLAYER_LORD_GODALMING);
+	if (currPlace == NOWHERE) return "ED";
+
+	PlaceId newPlace = currPlace;
+	if (predictLocation(hv) == NOWHERE) {
+		int numReturnedLocs;
+		PlaceId * places = HvWhereCanIGo(hv, &numReturnedLocs);
+		int num = rand() % numReturnedLocs;
+		newPlace = places[num];
+		free(places);
+	}
+	return placeIdToAbbrev(newPlace);
+}
+
+const char * decideDrSewardMove(HunterView hv) {
+	PlaceId currPlace = HvGetPlayerLocation(hv, PLAYER_DR_SEWARD);
+	if (currPlace == NOWHERE) return "BD";
+
+	PlaceId newPlace = currPlace;
+	if (predictLocation(hv) == NOWHERE) {
+		int numReturnedLocs;
+		PlaceId * places = HvWhereCanIGo(hv, &numReturnedLocs);
+		int num = rand() % numReturnedLocs;
+		newPlace = places[num];
+		free(places);
+	}
+	return placeIdToAbbrev(newPlace);
+}
+
+const char * decideVanHelsingMove(HunterView hv) {
+	PlaceId currPlace = HvGetPlayerLocation(hv, PLAYER_VAN_HELSING);
+	if (currPlace == NOWHERE) return "MA";
+
+	PlaceId newPlace = currPlace;
+	if (predictLocation(hv) == NOWHERE) {
+		int numReturnedLocs;
+		PlaceId * places = HvWhereCanIGo(hv, &numReturnedLocs);
+		int num = rand() % numReturnedLocs;
+		newPlace = places[num];
+		free(places);
+	}
+	return placeIdToAbbrev(newPlace);
+}
+
+const char * decideMinaHarkerMove(HunterView hv){
+	PlaceId currPlace = HvGetPlayerLocation(hv, PLAYER_MINA_HARKER);
+	if (currPlace == NOWHERE) 
+		return "CD";
+	PlaceId newPlace = CASTLE_DRACULA;
+	if (currPlace != CASTLE_DRACULA) {
+		int pathLength;
+		PlaceId * places = HvGetShortestPathTo(hv, PLAYER_MINA_HARKER, CASTLE_DRACULA, &pathLength);
+		newPlace = places[0];
+		free(places);
+	}
+	return placeIdToAbbrev(newPlace);
+
+}
+PlaceId predictLocation(HunterView hv) {
+	return NOWHERE;
+}
+
+//char * decideDrSewardMove(HunterView hv);
+//char * decideVanHelsingMove(HunterView hv);
+//char * decideMinaHarkerMove(HunterView hv);
