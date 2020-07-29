eafbbbcde1c736c0f2273098eaeb6b8886589e36
wenyaoc <wenyaochen427@gmail.com>
2020-07-29 19:33:46 +1000

    fix the style

--- view/HunterView.c
+++ view/HunterView.c
@@ -123,7 +123,7 @@ PlaceId HvGetLastKnownDraculaLocation(HunterView hv, Round *round) {
 	int number;
 	bool canFree;
 	PlaceId * place = GvGetLocationHistory(hv->gv, PLAYER_DRACULA, 
-                                           &number, &canFree);
+	                                       &number, &canFree);
 	PlaceId LastKnownPlace;
 	int known = 0;
 	for (int i = number - 1; i >= 0; i--) {
@@ -173,8 +173,9 @@ PlaceId *HvGetShortestPathTo(HunterView hv, Player hunter, PlaceId dest, int *pa
 		while (!QueueIsEmpty(q)) {
 			w = QueueLeave(q);
 			int numReturnedLocs;
-			PlaceId * reachable = GvGetReachable(hv->gv, hunter, round + dist[w], 
-                                                 w, &numReturnedLocs);
+			PlaceId * reachable = GvGetReachable(hv->gv, hunter, 
+			                                     round + dist[w], 
+			                                     w, &numReturnedLocs);
 			for (int i = 0; i < numReturnedLocs; i++) {
 				v = reachable[i];
 				if (w != v && pred[v] == -1) {
@@ -215,8 +216,9 @@ PlaceId *HvWhereCanIGoByType(HunterView hv, bool road, bool rail,
 	Player player = GvGetPlayer(hv->gv); // get current player
 	PlaceId from = GvGetPlayerLocation(hv->gv, player);
 	if (from == NOWHERE) return NULL; // hunter has no movement yet
-	return GvGetReachableByType(hv->gv, player, GvGetRound(hv->gv) + player, from, 
-                                road, rail, boat, numReturnedLocs);
+	return GvGetReachableByType(hv->gv, player, 
+	                            GvGetRound(hv->gv) + player, from, 
+	                            road, rail, boat, numReturnedLocs);
 }
 
 PlaceId *HvWhereCanTheyGo(HunterView hv, Player player,
@@ -234,8 +236,9 @@ PlaceId *HvWhereCanTheyGoByType(HunterView hv, Player player,
 		
 		Player currPlayer = GvGetPlayer(hv->gv);
 		if (currPlayer <= player) { // haven't made a movement in the current turn
-			return GvGetReachableByType(hv->gv, player, GvGetRound(hv->gv), from, 
-                                        road, rail, boat, numReturnedLocs);
+			return GvGetReachableByType(hv->gv, player, GvGetRound(hv->gv), 
+			                            from, road, rail, 
+			                            boat, numReturnedLocs);
 		} else { // the player already made a movement in the current turn
 			return GvGetReachableByType(hv->gv, player, GvGetRound(hv->gv) + 1, 
 			                            from, road, rail, boat, numReturnedLocs);
